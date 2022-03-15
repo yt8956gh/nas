8c18ba1765b8b45e0e933994a6ddef8f68e44d2e
Michael Mao <yt8956789.cs09g@nctu.edu.tw>
2022-03-15 11:51:08 +0000

    feat(nas): Finish ATSSS-Network-steering-functionalities-information

--- nasType/NAS_AtsssNetworkSteeringFuncInfo.go
+++ nasType/NAS_AtsssNetworkSteeringFuncInfo.go
@@ -1,13 +1,27 @@
 package nasType
 
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"net"
+)
+
 // TS 24.193 6.1.4
+
+const (
+	AtsssNetworkSteeringFuncInfoIpTypeIPv4 uint8 = iota + 1
+	AtsssNetworkSteeringFuncInfoIpTypeIPv6
+	AtsssNetworkSteeringFuncInfoIpTypeIPv4v6
+)
+
 type AtsssNetworkSteeringFuncInfo struct {
-	Ue3gppIpType        uint8
-	Ue3gppIpAddr        []byte
-	UeNon3gppIpType     uint8
-	UeNon3gppIpAddr     []byte
-	LenOfMptcpProxyInfo uint8
-	MptcpProxyInfoList  []MptcpProxyInfo
+	Ue3gppIpType       uint8
+	Ue3gppIpAddr       []byte
+	UeNon3gppIpType    uint8
+	UeNon3gppIpAddr    []byte
+	MptcpProxyInfoLen  uint8
+	MptcpProxyInfoList []MptcpProxyInfo
 }
 
 func NewAtsssNetworkSteeringFuncInfo() *AtsssNetworkSteeringFuncInfo {
@@ -18,15 +32,123 @@ func (a *AtsssNetworkSteeringFuncInfo) GetIdentifier() uint8 {
 	return AtsssParameterIdentifierNetworkSteeringfuncInfo
 }
 
-func (a *AtsssNetworkSteeringFuncInfo) Decode([]byte) error {
+func (a *AtsssNetworkSteeringFuncInfo) SetUe3gppIPv4Addr(ip net.IP) {
+	a.Ue3gppIpType = AtsssNetworkSteeringFuncInfoIpTypeIPv4
+	a.Ue3gppIpAddr = ip
+}
+
+func (a *AtsssNetworkSteeringFuncInfo) GetUe3gppIPv4Addr() net.IP {
+	return a.Ue3gppIpAddr
+}
+
+func (a *AtsssNetworkSteeringFuncInfo) SetUeNon3gppIPv4Addr(ip net.IP) {
+	a.UeNon3gppIpType = AtsssNetworkSteeringFuncInfoIpTypeIPv4
+	a.UeNon3gppIpAddr = ip
+}
+
+func (a *AtsssNetworkSteeringFuncInfo) GetUeNon3gppIPv4Addr() net.IP {
+	return a.UeNon3gppIpAddr
+}
+
+func (a *AtsssNetworkSteeringFuncInfo) SetMptcpProxyInfo(list []MptcpProxyInfo) {
+	a.MptcpProxyInfoList = list
+}
+
+func (a *AtsssNetworkSteeringFuncInfo) GetMptcpProxyInfo() []MptcpProxyInfo {
+	return a.MptcpProxyInfoList
+}
+
+func (a *AtsssNetworkSteeringFuncInfo) Decode(b []byte) error {
+	buffer := bytes.NewBuffer(b)
+	if err := binary.Read(buffer, binary.BigEndian, &a.Ue3gppIpType); err != nil {
+		return err
+	}
+	if a.Ue3gppIpType != AtsssNetworkSteeringFuncInfoIpTypeIPv4 {
+		return fmt.Errorf("Only support IPv4 in AtsssNetworkSteeringFuncInfo")
+	}
+	if err := binary.Read(buffer, binary.BigEndian, a.Ue3gppIpAddr[:4]); err != nil {
+		return err
+	}
+	if err := binary.Read(buffer, binary.BigEndian, &a.UeNon3gppIpType); err != nil {
+		return err
+	}
+	if a.UeNon3gppIpType != AtsssNetworkSteeringFuncInfoIpTypeIPv4 {
+		return fmt.Errorf("Only support IPv4 in AtsssNetworkSteeringFuncInfo")
+	}
+	if err := binary.Read(buffer, binary.BigEndian, a.UeNon3gppIpAddr[:4]); err != nil {
+		return err
+	}
+
+	if err := binary.Read(buffer, binary.BigEndian, &a.MptcpProxyInfoLen); err != nil {
+		return err
+	}
+
+	if buffer.Len() != int(a.MptcpProxyInfoLen) {
+		return fmt.Errorf("MptcpProxyInfoLen doesn't match the exact size.")
+	}
+
+	for buffer.Len() > 0 {
+		info := MptcpProxyInfo{}
+		if err := binary.Read(buffer, binary.BigEndian, &info.IpAddrType); err != nil {
+			return err
+		}
+		if info.IpAddrType != AtsssNetworkSteeringFuncInfoIpTypeIPv4 {
+			return fmt.Errorf("Only support IPv4 in AtsssNetworkSteeringFuncInfo")
+		}
+		if err := binary.Read(buffer, binary.BigEndian, info.IpAddr[:4]); err != nil {
+			return err
+		}
+		if err := binary.Read(buffer, binary.BigEndian, &info.Port); err != nil {
+			return err
+		}
+		if err := binary.Read(buffer, binary.BigEndian, &info.Type); err != nil {
+			return err
+		}
+		a.MptcpProxyInfoList = append(a.MptcpProxyInfoList, info)
+	}
+
 	return nil
 }
 
 func (a *AtsssNetworkSteeringFuncInfo) Encode() ([]byte, error) {
-	var result []byte
+	buffer := bytes.NewBuffer(nil)
+	if err := binary.Write(buffer, binary.BigEndian, &a.Ue3gppIpType); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(buffer, binary.BigEndian, a.Ue3gppIpAddr); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(buffer, binary.BigEndian, &a.UeNon3gppIpType); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(buffer, binary.BigEndian, a.UeNon3gppIpAddr); err != nil {
+		return nil, err
+	}
+	if err := binary.Write(buffer, binary.BigEndian, &a.MptcpProxyInfoLen); err != nil {
+		return nil, err
+	}
 
-	return result, nil
+	for _, info := range a.MptcpProxyInfoList {
+		if err := binary.Write(buffer, binary.BigEndian, &info.IpAddrType); err != nil {
+			return nil, err
+		}
+		if err := binary.Write(buffer, binary.BigEndian, info.IpAddr); err != nil {
+			return nil, err
+		}
+		if err := binary.Write(buffer, binary.BigEndian, &info.Port); err != nil {
+			return nil, err
+		}
+		if err := binary.Write(buffer, binary.BigEndian, &info.Type); err != nil {
+			return nil, err
+		}
+	}
+
+	return buffer.Bytes(), nil
 }
+
+const (
+	MptcpProxyTypeTransportConverter uint8 = 1
+)
 
 type MptcpProxyInfo struct {
 	IpAddrType uint8
